6abdb13c8691398bd1f2714b54a7193f1bf0ed20
Baaska <macbookpro@MacBook-Pro.local>
2024-01-24 13:08:56 +0800

    Application blade

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Exegroup') }}</title>
+    <title>{{ config('app.name', 'Exe Group') }}</title>
     <link href="/images/icon.ico" rel="icon" type="image/ico" sizes="16x16" />
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
